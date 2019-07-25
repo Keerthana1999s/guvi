f4ec58697ea8a56d7f304a19266565444c2199f3
Keerthana1999s <51355394+Keerthana1999s@users.noreply.github.com>
2019-07-25 21:56:24 +0530

    Update binary.py

--- code/binary.py
+++ code/binary.py
@@ -1,8 +1,8 @@
 a=int(input())
-m=[]*n
-for i in range(0,2**n):
+m=[]*a
+for i in range(0,2**a):
     k=[]
-    for j in range(n-1,0,-1):
+    for j in range(a-1,0,-1):
         k.append(int(i/2**j)%2)
     k.append(i%2)    
     m.append(k) 
