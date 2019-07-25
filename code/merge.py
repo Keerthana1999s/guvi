8463d52a7d33daaf63bc09dfb905293296e48061
Keerthana1999s <51355394+Keerthana1999s@users.noreply.github.com>
2019-07-25 20:51:37 +0530

    Create merge.py

---
+++ code/merge.py
@@ -0,0 +1,14 @@
+a=int(input())
+m=[]
+for i in range(a):
+    l=[]
+    l=list(map(int,input().split()))
+    m.append(l)
+ 
+s=[]   
+for i in m:
+    for j in i:
+        s.append(j)
+s.sort()
+for i in s:
+    print(i,end=" ")
